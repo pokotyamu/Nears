d6f88a7d0d78329db6b8cec4fb981feb768855da
pokotyamu <pokotyamu@gmail.com>
2014-12-31 16:40:39 +0900

    fin tutorial

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,9 +2,8 @@ class MapController < ApplicationController
   def index
     @places = Place.all
     @hash = Gmaps4rails.build_markers(@places) do |place, marker|
-            marker.lat place.lat
-            marker.lng place.lon
-            marker.infowindow user.description
+            marker.lat place.latitude
+            marker.lng place.longitude
             marker.json({title: place.title})
     end
   end
