808cfa2ff690312f2ae98e727dae13fa4aacf6eb
pokotyamu <pokotyamu@gmail.com>
2015-01-07 16:37:40 +0900

    devise installed

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'map/' => 'map#index' 
 
   resources :places
-
+  root to: "map#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
