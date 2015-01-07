49ddcd535c4d5f00bcd91f0e4f31e49cd22d7635
pokotyamu <pokotyamu@gmail.com>
2015-01-07 19:23:47 +0900

    show user only place

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,6 +1,10 @@
 class MapController < ApplicationController
   def index
-    @places = Place.all
+    if user_signed_in?
+      @places = Place.where(username: current_user.username) 
+    else
+      @places = Place.all
+    end
     @hash = Gmaps4rails.build_markers(@places) do |place, marker|
             marker.lat place.latitude
             marker.lng place.longitude
